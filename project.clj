645ec25ec5b8cc1dedbc729a3dd78fd3728311f0
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-21 00:20:45 +0000

    Added dependencies

--- project.clj
+++ project.clj
@@ -4,13 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1586"]
+                 [org.clojure/data.json "0.2.1"]
+                 [clj-http "0.6.5"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.2"]
                  [jayq "2.3.0"]
-                 [rm-hull/monet "0.1.6-SNAPSHOT"]]
+                 [rm-hull/monet "0.1.6-SNAPSHOT"]
+                 [rm-hull/turtle "0.1.0-SNAPSHOT"]] 
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler enchilada.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}}
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.1.0"
   :warn-on-reflection true)
