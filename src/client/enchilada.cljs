5c0d8a3f8a80defb557304d4bdbf417307ff90c0
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-22 23:22:50 +0000

    Missing reference

--- src/client/enchilada.cljs
+++ src/client/enchilada.cljs
@@ -1,5 +1,6 @@
 (ns enchilada
-  (:use [jayq.core :only [$]]))
+  (:use [monet.canvas :only [get-context]]
+        [jayq.core :only [$]]))
 
 (def canvas ($ :canvas#world)) 
 (def ctx    (get-context (.get canvas 0) "2d")) 
