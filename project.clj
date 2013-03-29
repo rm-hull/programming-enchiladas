91f73a30ee799e9a8c5056b25ac8893f24409207
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-29 23:22:26 +0000

    Bump turtle library version

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [jayq "2.3.0"]
                  [com.novemberain/monger "1.5.0-rc1"]
                  [rm-hull/monet "0.1.7"]
-                 [rm-hull/turtle "0.1.2"]] 
+                 [rm-hull/turtle "0.1.3"]] 
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler enchilada.handler/app}
   :profiles
