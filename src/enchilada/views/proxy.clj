6f07532c7d759f4a25562dcf82bfd508ee807511
Richard Hull <rm_hull@yahoo.co.uk>
2013-05-01 21:56:50 +0100

    Copy headers from proxied response; Coerce proxied response body as a byte array, and stream as a byte array

--- src/enchilada/views/proxy.clj
+++ src/enchilada/views/proxy.clj
@@ -1,17 +1,25 @@
 (ns enchilada.views.proxy
   (:use [compojure.core :only [defroutes GET]]
-        [ring.util.response :only [content-type response]])
+        [ring.util.response :only [header response]])
   (:require [clj-http.client :as http]))
 
+(defn- add-headers [response headers]
+  (if (nil? headers)
+    response
+    (let [item (first headers)]
+      (recur 
+        (header response (key item) (val item))
+        (next headers)))))
+
 (defn proxy-request [url]
-   (let [{:keys [status headers body] :as resp} (http/get url)]
-      (println headers)
+   (let [{:keys [status headers body]} (http/get url {:as :byte-array})]
       (if (= status 200)
         (->
-          (response body)
-          (content-type (:content-type headers))))))
-; need to replicate all/most of the returned headers
+          (response (java.io.ByteArrayInputStream. body))
+          (add-headers headers)
+          (header "x-proxied-by" "Programming Enchiladas")))))
 
 (defroutes routes
   (GET "/proxy" [:as req]
     (proxy-request (get-in req [:params :url]))))
+
