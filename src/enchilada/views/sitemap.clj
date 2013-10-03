424bc434fcada325b9e31980be22eef6436fc669
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-03 14:24:54 +0100

    Fix sitemap problem with js maps

--- src/enchilada/views/sitemap.clj
+++ src/enchilada/views/sitemap.clj
@@ -8,8 +8,13 @@
   (:require [me.raynes.fs :as fs])
   (:import [java.io File]))
 
+(defn is-compressed-js? [^File f]
+  (and
+    (fs/file? f)
+    (.endsWith (.getName f) ".gz")))
+
 (defn work-files* [^File dir]
-  (filter fs/file? (file-seq dir)))
+  (filter is-compressed-js? (file-seq dir)))
 
 (defn parent-and-name [^File f]
   (str (fs/name (fs/parent f)) "/" (fs/name f)))
@@ -25,11 +30,11 @@
 (defn generate-sitemap [base-url work-dir]
   (sexp-as-element
     [:urlset {:xmlns "http://www.sitemaps.org/schemas/sitemap/0.9"}
-      (map (url-element base-url :weekly 0.5) (work-files* work-dir))])) 
+      (map (url-element base-url :weekly 0.5) (work-files* work-dir))]))
 
 (defroutes routes
   (GET "/sitemap.xml" [:as req]
     (let [base-url (str (name (:scheme req)) "://" (:server-name req) ":" (:server-port req))]
       (->
-        (response (emit-str (generate-sitemap base-url (file "work/gists/out")))) 
+        (response (emit-str (generate-sitemap base-url (file "work/gists/out"))))
         (content-type "text/xml")))))
