64f3313d487c7205f0b533ddf08544131ba96786
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-06 23:58:20 +0100

    Temporarily redirect / to sitemap page

--- src/enchilada/views/welcome.clj
+++ src/enchilada/views/welcome.clj
@@ -1,9 +1,11 @@
 (ns enchilada.views.welcome
   (:use [compojure.core :only [defroutes GET]]
-        [hiccup.core :only [html]] 
-        [hiccup.element :only [image link-to]] 
+        [ring.util.response :only [redirect]]
+        [hiccup.core :only [html]]
+        [hiccup.element :only [image link-to]]
         [hiccup.page :only [include-js]]))
 
-(defroutes routes 
-  (GET "/" [] "Welcome - TODO"))
+(defroutes routes
+  ;(GET "/" [] "Welcome - TODO"))
+  (GET "/" [] (redirect "/sitemap.xml")))
 
