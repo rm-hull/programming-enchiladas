5d9bb7f944181e20f8cf2ab35152e6ff5b2b085d
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-07 10:07:10 +0100

    Robots file

--- src/enchilada/views/welcome.clj
+++ src/enchilada/views/welcome.clj
@@ -1,11 +1,12 @@
 (ns enchilada.views.welcome
   (:use [compojure.core :only [defroutes GET]]
-        [ring.util.response :only [redirect]]
+        [ring.util.response :only [redirect file-response]]
         [hiccup.core :only [html]]
         [hiccup.element :only [image link-to]]
         [hiccup.page :only [include-js]]))
 
 (defroutes routes
   ;(GET "/" [] "Welcome - TODO"))
+  (GET "/robots.txt" [] (file-response "resources/private/robots.txt"))
   (GET "/" [] (redirect "/sitemap.xml")))
 
