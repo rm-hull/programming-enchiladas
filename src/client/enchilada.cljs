dff067d9843e0b72788471de7305080a4b05dd1c
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-22 15:54:17 +0100

    Added useful command-line params

--- src/client/enchilada.cljs
+++ src/client/enchilada.cljs
@@ -14,4 +14,26 @@
        (show console)
        (append console (str "<p>" s "</p>"))))
 
+(defn- pair
+  "Pulls apart a string separated by a = into constituent vector parts,
+   coverts the key into a keyword"
+  [s]
+  (let [[k v] (split s #"=")]
+    [(keyword k) v]))
+
+(defn get-params
+  "Extracts a set of key-value pairs separated by & into a map"
+  [s]
+  (let [drop1 (apply str (drop 1 (seq s)))]
+    (->>
+      (split drop1 #"&")
+      (map pair)
+      (into {}))))
+
+(defn value-of [k default]
+  (let [params (get-params (.-search (.-location js/window)))]
+    (get params k default)))
+
+(def params (get-params (.-search (.-location js/window))))
+
 (hide ($ :div#spinner))
