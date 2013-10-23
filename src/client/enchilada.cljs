c658fdfc351cf2c30583a7e7175d5ab36b8cad18
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-23 09:00:41 +0100

    Added canvas-size function

--- src/client/enchilada.cljs
+++ src/client/enchilada.cljs
@@ -8,13 +8,18 @@
 (def canvas ($ :canvas#world))
 (def ctx    (get-context (.get canvas 0) "2d"))
 (def svg    ($ :svg#world))
-(defn proxy-request [url] (str "/proxy?url=" (js/encodeURI url)))
 
 (set! *print-fn*
      (fn [s]
        (.log js/console s)
        (show console)
        (append console (str "<p>" s "</p>"))))
+
+(defn proxy-request [url]
+  (str "/proxy?url=" (js/encodeURI url)))
+
+(defn canvas-size []
+  [(.width canvas) (.height canvas)])
 
 (defn- pair
   "Pulls apart a string separated by a = into constituent vector parts,
@@ -23,7 +28,7 @@
   (let [[k v] (split s #"=")]
     [(keyword k) v]))
 
-(defn get-params
+(defn- get-params
   "Extracts a set of key-value pairs separated by & into a map"
   [s]
   (let [drop1 (apply str (drop 1 (seq s)))]
@@ -36,6 +41,7 @@
   (let [params (get-params (.-search (.-location js/window)))]
     (get params k default)))
 
-(def params (get-params (.-search (.-location js/window))))
+(defn url-params []
+  (get-params (.-search (.-location js/window))))
 
 (hide ($ :div#spinner))
