6a3027b015cd80733a7039e92c90cdc9dbea24b5
Richard Hull <rm_hull@yahoo.co.uk>
2014-01-19 00:00:32 +0000

    Fix version of core.async

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/data.json "0.2.3"]
                  [org.clojure/data.xml "0.0.7"]
                  [tailrecursion/cljs-priority-map "1.0.3"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/core.logic "0.8.5"]
                  ;[org.clojure/core.match "0.2.0"]
                  [org.clojure/core.typed "0.2.21"]
