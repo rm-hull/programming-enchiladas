440c2ef4aa6a5c80e49439301483407bd8eba3b7
Richard Hull <rm_hull@yahoo.co.uk>
2014-03-08 21:03:48 +0000

    No async loading for maps

--- src/enchilada/util/google_analytics.clj
+++ src/enchilada/util/google_analytics.clj
@@ -1,7 +1,7 @@
 (ns enchilada.util.google-analytics
   (:require
     [clojure.string :as str]
-    [enchilada.util.page :refer [include-async-js]]))
+    [hiccup.page :refer [include-js]]))
 
 (def template
   "<script>
@@ -28,7 +28,5 @@
   (memoize
     (fn [api-key sensor?]
       (when-not (empty? api-key)
-        (include-async-js
+        (include-js
           (str "https://maps.googleapis.com/maps/api/js?key=" api-key "&sensor=" sensor?))))))
-
-
