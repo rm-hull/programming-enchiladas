4c53e9f0ca45ed4e8e5bf9469bb599d4247f3520
Richard Hull <rm_hull@yahoo.co.uk>
2014-06-04 20:16:19 +0100

    Google analytics stuff

--- src/enchilada/util/google_analytics.clj
+++ src/enchilada/util/google_analytics.clj
@@ -11,6 +11,7 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'TRACKING_ID', 'SITE_URL');
+  ga('require', 'linkid', 'linkid.js');
   ga('send', 'pageview');
 
 </script>")
